33aa2ad7980d350aa92731efb41455257603d1dc
Chun-Ho (Hugo) Lin <hugo970217@gmail.com>
2025-01-24 03:19:20 +0800

    feat: add a hero section to the homepage, update metadata and icons, and revise the footer (#2)
    Co-authored-by: tszhong0411 <me@honghong.me>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,142 @@
-import ThemeToggle from "../components/theme/theme-toggle";
+'use client'
+
+import { Input } from "@/components/ui/input"
+import {
+  FileTextIcon,
+  type LucideIcon,
+  PencilLine,
+  Network,
+} from 'lucide-react'
+import React from 'react'
+import Hero from "@/components/hero";
+import Link from "next/link";
+
+type CardProps = {
+  tools: Tool[]
+  title: string
+}
+
+type Tool = {
+  label: string
+  link: string
+  keywords: string[]
+  color: string
+  icon: LucideIcon
+}
+
+const TOOLS = [
+  {
+    label: 'Generator',
+    links: [
+      {
+        label: 'E-mail Signature Generator',
+        link: '/email-signature',
+        keywords: ['Generator', 'Password', 'Random'],
+        color: '#f783ac',
+        icon: PencilLine
+      }
+    ]
+  },
+  {
+    label: 'Network',
+    links: [
+      {
+        label: 'Subnet Calculator',
+        link: '/',
+        keywords: ['PDF', 'Viewer', 'Document', 'File', 'Document'],
+        color: '#15aabf',
+        icon: Network
+      }
+    ]
+  },
+  {
+    label: 'Editor',
+    links: [
+      {
+        label: 'Markdown Previewer',
+        link: '/calculation/word-counter',
+        keywords: ['Word', 'Counter', 'Calculation', 'Text'],
+        color: '#4c6ef5',
+        icon: FileTextIcon
+      }
+    ]
+  },
+]
+
 
 const Home = () => {
-  return (
-    
-    <main>
-      <section className="flex min-h-dvh flex-col items-center justify-center space-y-5">
-        <h1 className="text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Next.js + Tailwind CSS Template
-        </h1>
+  const [value, setValue] = React.useState('')
 
-        <ThemeToggle />
-      </section>
-    </main>
+  const filter = (tool: Tool): boolean =>
+    tool.label.toLowerCase().includes(value.toLowerCase()) ||
+    tool.keywords.some((keyword) =>
+      keyword.toLowerCase().includes(value.toLowerCase())
+    )
+
+  return (
+    <div>
+      <Hero />
+      <div className='flex flex-col items-start'>
+        <Input
+          type='text'
+          value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
+          placeholder='Search'
+          className='w-full'
+        />
+        <div
+          id='get-started'
+          className='my-12 flex w-full scroll-mt-20 flex-col gap-6'
+        >
+          {value
+            ? TOOLS.filter((t) => t.links.some((tool) => filter(tool))).map(
+                (t) => {
+                  const { label, links } = t
+                  const filtered = links.filter((tool) => filter(tool))
+
+                  return <Card key={label} tools={filtered} title={label} />
+                }
+              )
+            : TOOLS.map((tool) => {
+                const { label, links } = tool
+
+                return <Card key={label} tools={links} title={label} />
+              })}
+        </div>
+      </div>
+    </div>
   );
 };
+
+const Card = (props: CardProps) => {
+  const { tools, title } = props
+
+  return (
+    <div className='w-full rounded-lg border p-4'>
+      <div>{title}</div>
+      <div className='mt-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3'>
+        {tools.map((tool) => (
+          <Item key={tool.label} {...tool} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const Item = (props: Tool) => {
+  const { color, icon, label, link } = props
+  const Icon = icon
+
+  return (
+    <Link
+      href={link}
+      className='flex flex-col items-center justify-center rounded-lg bg-accent p-4 text-center transition-colors duration-300 hover:bg-accent-highlight'
+    >
+      <Icon color={color} size={32} />
+      <div className='mt-1.5'>{label}</div>
+    </Link>
+  )
+}
+
 
 export default Home;
