f87ee4d718ed6f87af5c23b6b2e94886f23952c6
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-25 19:03:24 +0800

    refactor(page): standardize string quotes and improve code consistency and update icon of paranoid-text-spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-'use client'
+"use client"
 
 import { Input } from "@/components/ui/input"
 import {
   FileTextIcon,
   PencilLine,
   Network,
-} from 'lucide-react'
-import type { LucideIcon } from 'lucide-react'
-import React from 'react'
+  Ruler,
+} from "lucide-react"
+import type { LucideIcon } from "lucide-react"
+import React from "react"
 import Hero from "@/components/hero";
 import Link from "next/link";
 
@@ -26,53 +27,53 @@ type Tool = {
 
 const TOOLS = [
   {
-    label: 'Editor',
+    label: "Editor",
     links: [
       {
-        label: 'Markdown Previewer',
-        link: '/markdown-previewer',
-        keywords: ['Word', 'Counter', 'Calculation', 'Text'],
+        label: "Markdown Previewer",
+        link: "/markdown-previewer",
+        keywords: ["Word", "Counter", "Calculation", "Text"],
         color: "#fa5252",
-        icon: FileTextIcon
+        icon: FileTextIcon,
       },
       {
-        label: 'Paranoid Text Spacing',
-        link: '/paranoid-text-spacing',
-        keywords: ['Word', 'Counter', 'Calculation', 'Text'],
+        label: "Paranoid Text Spacing",
+        link: "/paranoid-text-spacing",
+        keywords: ["Word", "Counter", "Calculation", "Text"],
         color: "#40c057",
-        icon: FileTextIcon
+        icon: Ruler,
       },
     ]
   },
   {
-    label: 'Generator',
+    label: "Generator",
     links: [
       {
-        label: 'E-mail Signature Generator',
-        link: '/email-signature',
-        keywords: ['Generator', 'Password', 'Random'],
-        color: '#f783ac',
-        icon: PencilLine
-      }
+        label: "E-mail Signature Generator",
+        link: "/email-signature",
+        keywords: ["Generator", "Password", "Random"],
+        color: "#f783ac",
+        icon: PencilLine,
+      },
     ]
   },
   {
-    label: 'Network',
+    label: "Network",
     links: [
       {
-        label: 'Subnet Calculator',
-        link: '/#get-started',
-        keywords: ['PDF', 'Viewer', 'Document', 'File', 'Document'],
-        color: '#15aabf',
-        icon: Network
-      }
+        label: "Subnet Calculator",
+        link: "/#get-started",
+        keywords: ["PDF", "Viewer", "Document", "File", "Document"],
+        color: "#15aabf",
+        icon: Network,
+      },
     ]
   },
 ]
 
 
 function Home() {
-  const [value, setValue] = React.useState('')
+  const [value, setValue] = React.useState("")
 
   const filter = (tool: Tool): boolean =>
     tool.label.toLowerCase().includes(value.toLowerCase()) ||
@@ -83,17 +84,17 @@ function Home() {
   return (
     <div>
       <Hero />
-      <div className='flex flex-col items-start'>
+      <div className="flex flex-col items-start">
         <Input
-          type='text'
+          type="text"
           value={value}
           onChange={(e) => setValue(e.currentTarget.value)}
-          placeholder='Search'
-          className='w-full'
+          placeholder="Search"
+          className="w-full"
         />
         <div
-          id='get-started'
-          className='my-12 flex w-full scroll-mt-20 flex-col gap-6'
+          id="get-started"
+          className="my-12 flex w-full scroll-mt-20 flex-col gap-6"
         >
           {value
             ? TOOLS.filter((t) => t.links.some((tool) => filter(tool))).map(
@@ -117,9 +118,9 @@ function Home() {
 
 function Card({ tools, title }: CardProps) {
   return (
-    <div className='w-full rounded-lg border p-4'>
+    <div className="w-full rounded-lg border p-4">
       <div>{title}</div>
-      <div className='mt-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3'>
+      <div className="mt-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3">
         {tools.map((tool) => (
           <Item key={tool.label} {...tool} />
         ))}
@@ -134,10 +135,10 @@ function Item({ color, icon, label, link }: Tool) {
   return (
     <Link
       href={link}
-      className='flex flex-col items-center justify-center rounded-lg bg-accent p-4 text-center transition-colors duration-300 hover:bg-accent-highlight'
+      className="flex flex-col items-center justify-center rounded-lg bg-accent p-4 text-center transition-colors duration-300 hover:bg-accent-highlight"
     >
       <Icon color={color} size={32} />
-      <div className='mt-1.5'>{label}</div>
+      <div className="mt-1.5">{label}</div>
     </Link>
   )
 }
